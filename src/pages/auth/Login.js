052b7efe60c9571a811610d24cb0b768e6b41a7a
Leland Henderson <lelandhenderson31415@gmail.com>
2024-12-31 13:57:44 -0800

    completed structure for Login page

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -3,14 +3,28 @@ import LoginForm from "../../components/shared/LoginForm";
 const Login = () => {
   return (
     <div className="login-page">
-      <div className="login-title">My budget</div>
+      <div className="login-title">
+        <h1>My budget</h1>
+      </div>
       <div className="login-form">
         <LoginForm />
       </div>
       <div className="login-button">
         <Button title="Login" />
       </div>
-      <div className="login-options">login options</div>
+      <div className="login-options">
+        <p>
+          <a href="https://www.facebook.com" className="custom-link">
+            Register
+          </a>
+        </p>
+        <p>
+          reset{" "}
+          <a href="https://www.facebook.com" className="custom-link">
+            password
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
