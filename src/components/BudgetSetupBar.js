e5427469dcf02769708a512f61ef2f507195edc0
Leland Henderson <lelandhenderson31415@gmail.com>
2025-01-17 20:02:43 -0800

    structured budget setup bar

--- src/components/BudgetSetupBar.js
+++ src/components/BudgetSetupBar.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import BudgetDisplayBar from "./BudgetDisplayBar.js";
-
+import Button from "../components/shared/Button.js";
 const BudgetSetupBar = () => {
   const [goalAmount, setGoalAmount] = useState();
+  const [startingAmount, setStartingAmount] = useState();
   const [expense, setExpense] = useState();
   const [expenseType, setExpenseType] = useState();
   const [income, setIncome] = useState();
@@ -18,36 +19,70 @@ const BudgetSetupBar = () => {
   };
   return (
     <div className="budget-setup">
-      <h1>Budget Setup</h1>
+      <div style={{ alignSelf: "center" }}>
+        <h1>Budget Setup</h1>
+        <h2>Create New Budget</h2>
+      </div>
 
-      <label>Savings Goal</label>
-      <input
-        placeholder="Savings goal"
-        value={goalAmount}
-        className="goal-amount-input"
-        onChange={(e) => setGoalAmount(e.target.value)}
-      />
-      <label htmlFor="expense">Expense</label>
       <div>
-        <input
-          id="expense"
-          placeholder="New Expense"
-          value={expense}
-          className="new-expense-input"
-          onChange={(e) => setExpense(e.target.value)}
-        />
-        <select
-          className="expense-income-type"
-          value={expenseType}
-          onChange={(e) => setExpenseType(e.target.value)}
-        >
-          <option value="">bill</option>
-          <option value="">grocery</option>
-          <option value="">other(unrequired)</option>
-          <option value="">other(required)</option>
-        </select>
+        <div className="savings_goal">
+          <label>Savings Goal:</label>
+          <input
+            placeholder="Savings goal"
+            value={goalAmount}
+            className="goal-amount-input"
+            onChange={(e) => setGoalAmount(e.target.value)}
+          />
+        </div>
+        <div className="savings_goal">
+          <label>Starting Amount:</label>
+          <input
+            placeholder="Starting Amount"
+            value={startingAmount}
+            className="goal-amount-input"
+            onChange={(e) => setStartingAmount(e.target.value)}
+          />
+        </div>
+      </div>
+      <Button>Submit</Button>
+      <div style={{ alignSelf: "center" }}>
+        <h2>Add Expense</h2>
       </div>
       <div>
+        <div className="add-expense">
+          <label htmlFor="expense">Description:</label>
+
+          <input
+            id="expense"
+            placeholder="New Expense"
+            value={expense}
+            className="new-expense-input"
+            onChange={(e) => setExpense(e.target.value)}
+          />
+        </div>
+        <div className="add-expense">
+          <label htmlFor="expense-type">Type:</label>
+          <select
+            id="expense-type"
+            className="expense-income-type"
+            value={expenseType}
+            onChange={(e) => setExpenseType(e.target.value)}
+          >
+            <option value="">bill</option>
+            <option value="">grocery</option>
+            <option value="">other(unrequired)</option>
+            <option value="">other(required)</option>
+          </select>
+        </div>
+      </div>
+      <div
+        style={{
+          paddingLeft: 20,
+          paddingRight: 20,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <label>
           <input
             type="radio"
@@ -67,16 +102,23 @@ const BudgetSetupBar = () => {
           Non-Recurring
         </label>
       </div>
+      <Button>Submit</Button>
+      <div style={{ alignSelf: "center" }}>
+        <h2>Add Income</h2>
+      </div>
+      <div className="add-expense">
+        <label htmlFor="income">Description:</label>
 
-      <label htmlFor="income">Income</label>
-      <div>
         <input
           id="income"
           placeholder="New Income"
           value={income}
-          className="new-income-input"
+          className="new-expense-input"
           onChange={(e) => setIncome(e.target.value)}
         />
+      </div>
+      <div className="add-expense">
+        <label htmlFor="income-type">Type:</label>
         <select
           className="expense-income-type"
           value={incomeType}
@@ -86,7 +128,14 @@ const BudgetSetupBar = () => {
           <option value="">Side gig</option>
         </select>
       </div>
-      <div>
+      <div
+        style={{
+          paddingLeft: 20,
+          paddingRight: 20,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
         <label>
           <input
             type="radio"
@@ -106,6 +155,8 @@ const BudgetSetupBar = () => {
           Non-Recurring
         </label>
       </div>
+      <Button>Submit</Button>
+
       <BudgetDisplayBar />
     </div>
   );
